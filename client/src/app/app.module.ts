74778340607a96ee02a86f6ac713b388d0797b1f
Luckas Rakoto <luckasrakoto@gmail.com>
2025-02-01 11:56:10 -0500

    form init

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -30,6 +30,7 @@ import { MatStepperModule } from '@angular/material/stepper';
 import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip';
+import { FormComponent } from './pages/form/form.component';
 
 const modules = [
   MatMenuModule,
@@ -63,7 +64,7 @@ const modules = [
 ];
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, FormComponent],
   imports: [
     ...modules,
     BrowserModule,
